f1b1508ad7d3cbf0637d62031424bec0ba4078a6
Tanvir Ali <Tanvir.Ali@dentsu.com>
2021-04-30 11:20:37 +0100

    local storage now saves selected books

--- src/js/functions/bookEvents.js
+++ src/js/functions/bookEvents.js
@@ -11,9 +11,27 @@ export const bookEvents = () => {
 		const allBooks = [ ...document.querySelectorAll('.book-item') ];
 
 		// loop through all book
-
 		for (const books of allBooks) {
-			console.log(books);
+			const title = books.querySelector('h3');
+
+			// check localstorage
+			// If local storage has the title of the book then add class
+			if (localStorage.getItem(title.textContent)) {
+				books.classList.add('is-selected');
+			} else {
+				books.classList.remove('is-selected');
+			}
+
+			// add click event listener
+			books.addEventListener('click', () => {
+				if (!books.classList.contains('is-selected')) {
+					books.classList.add('is-selected');
+					localStorage.setItem(title.textContent, true);
+				} else {
+					books.classList.remove('is-selected');
+					localStorage.removeItem(title.textContent);
+				}
+			});
 		}
 	});
 };
